a989129bd9ca8c7aa4d812dd5650d7869e11edaa
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-06-06 16:00:48 -0700

    add normalize.css

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import routes from './routes';
 import configureStore from './store/configureStore';
 
 require('./favicon.ico'); // Tell webpack to load favicon.ico
+import '../node_modules/normalize.css/normalize.css';
 import '../node_modules/milligram/dist/milligram.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
