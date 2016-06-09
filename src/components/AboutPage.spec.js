27241cd432dcc2e64489295acb38e0a131f8424e
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-06-08 18:42:23 -0700

    remove test that is no longer needed

--- src/components/AboutPage.spec.js
+++ src/components/AboutPage.spec.js
@@ -12,13 +12,13 @@ describe('<AboutPage />', () => {
     expect(actual).to.equal(expected);
   });
 
-  it('should have a header with \'alt-header\' class', () => {
-    const wrapper = shallow(<AboutPage />);
-    const actual = wrapper.find('h2').prop('className');
-    const expected = 'alt-header';
-
-    expect(actual).to.equal(expected);
-  });
+  // it('should have a header with \'alt-header\' class', () => {
+  //   const wrapper = shallow(<AboutPage />);
+  //   const actual = wrapper.find('h2').prop('className');
+  //   const expected = 'alt-header';
+  //
+  //   expect(actual).to.equal(expected);
+  // });
 
   it('should link to an unknown route path', () => {
     const wrapper = shallow(<AboutPage />);
