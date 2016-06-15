5c3a7d30aeadbde5eb60b3b05e9454aa7367faf1
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-06-14 20:25:44 -0700

    add method for navigating to named route

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,8 @@ import AccountPage from './modules/account/containers/AccountPage';
 import FriendsPage from './modules/friends/containers/FriendsPage';
 import ManageFriendPage from './modules/friends/containers/ManageFriendPage';
 
+import AddArticlePage from './modules/articles/containers/AddArticlePage';
+
 
 export default (
   <Route path="/" component={App}>
@@ -18,6 +20,7 @@ export default (
 
     <Route path="friends" component={FriendsPage}/>
     <Route path="friend" component={ManageFriendPage}/>
+    <Route path="friend/:id/article" component={AddArticlePage}/>
     <Route path="friend/:id" component={ManageFriendPage}/>
 
     <Route path="account" component={AccountPage}/>
